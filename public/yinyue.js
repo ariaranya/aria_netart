9329fae57a62c4378a89c1e1f9f8663121ef8ae1
aranyaria <s3713486@student.rmit.edu.au>
2023-05-22 17:02:37 +1000

    yinyue2

--- public/yinyue.js
+++ public/yinyue.js
@@ -1,5 +1,28 @@
-// 创建 Audio 对象并指定音乐文件路径
-const audio = new Audio('bg_music.mp3');
+// 创建 AudioContext 对象
+const audioContext = new AudioContext();
+
+// 加载音乐文件
+function loadMusic(file) {
+  return fetch(file)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer));
+}
 
 // 播放音乐
-audio.play();
+function playMusic(buffer) {
+  const source = audioContext.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioContext.destination);
+  source.start(0);
+}
+
+// 在适当的地方调用加载和播放音乐的函数
+loadMusic('bg_music.mp3')
+  .then(buffer => {
+    // 加载成功，可以播放音乐
+    playMusic(buffer);
+  })
+  .catch(error => {
+    // 加载失败，处理错误
+    console.error('Failed to load music:', error);
+  });
